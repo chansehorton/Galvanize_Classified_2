1e7ad41c64a41bed6483b6180b427f5ce5305b09
CHANSE HORTON <chansehorton@yahoo.com>
2017-01-31 16:55:07 -0700

    heroku changes 12

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ const bodyParser = require('body-parser');
 
 const messages = require('./app/routes/classifieds');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/app/public')));
 
 app.use(bodyParser.json());
 
 app.use('/classifieds', messages);
 
 app.use('*', function(req, res, _next) {
-  res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+  res.sendFile('index.html', {root: path.join(__dirname, '/app/public')});
 });
 
 app.listen(port, () => {
