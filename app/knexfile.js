1b0487a59495b1d5dbea520b28e7846a2d46f203
CHANSE HORTON <chansehorton@yahoo.com>
2017-01-31 15:54:25 -0700

    more heroku deployment changes

--- app/knexfile.js
+++ app/knexfile.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: 'postgres://immpttxlvtilxe:efc889f8fa42685cc2e1b37f9d62bc938cbeb9ef86953052b99ae1a5dc4b4e65@ec2-54-163-240-7.compute-1.amazonaws.com:5432/d4ihfd9b86cuvm'
+    connection: process.env.DATABASE_URL
   },
 
 };
